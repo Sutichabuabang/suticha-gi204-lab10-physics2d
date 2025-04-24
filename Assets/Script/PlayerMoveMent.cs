1476b4b58dfb7e9874b437a9c18f1957152dffd4
WayuSuradech <sutichabuabang@gmail.com>
2025-04-24 22:47:21 +0700

    Finish

--- Assets/Script/PlayerMoveMent.cs
+++ Assets/Script/PlayerMoveMent.cs
@@ -7,6 +7,7 @@ public class PlayerM : MonoBehaviour
     public float JumpForce;
     public bool IsJumping;
     Rigidbody2D rb2d;
+    Vector2 moveInput;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,8 +18,10 @@ public class PlayerM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move = Input.GetAxis("Horizontal");
-        rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.AddForce(moveInput * Speed);
+        /*move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);*/
         if (Input.GetButtonDown("Jump")&& !IsJumping)
         {
             rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, JumpForce));
